609351dad9d8cd5cba09cd794f21cba3aba269f0
Victória Barros <viccruzbarros@gmail.com>
2025-05-13 21:41:09 -0300

    update readme.md

---
+++ src/app/_components/about.tsx
@@ -0,0 +1,77 @@
+import Image from "next/image";
+import about1Img from '../../../public/about-1.png'
+import { Check, MapPin } from "lucide-react";
+import { WhatsappLogo } from "@phosphor-icons/react/dist/ssr";
+
+export function About() {
+  return (
+    <section className="bg-[#FDF6ec] py-16">
+      <div className="container px-4 mx-auto">
+
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
+
+          <div className="relative" data-aos="fade-up-right" data-aos-delay="300">
+
+            <div className="relative w-full h-[400px] rounded-3xl overflow-hidden">
+              <Image
+                src={about1Img}
+                alt="Foto do cachorro"
+                fill
+                quality={100}
+                className="object-cover hover:scale-110 duration-300"
+                priority
+              />
+            </div>
+
+          </div>
+
+          <div className="space-y-6 mt-10" data-aos="fade-up-left" data-aos-delay="300">
+            <h2 className="text-4xl font-bold">SOBRE</h2>
+
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+
+            <ul className="space-y-4">
+              <li className="flex items-center gap-2">
+                <Check className="text-red-500" />
+                Atuamos com paixão e dedicação desde 1998.
+              </li>
+              <li className="flex items-center gap-2">
+                <Check className="text-red-500" />
+                Equipe com mais de 10 veterinários.
+              </li>
+              <li className="flex items-center gap-2">
+                <Check className="text-red-500" />
+                Atendimento de excelência para garantir a saúde e bem-estar do seu pet.
+              </li>
+            </ul>
+
+            <div className="flex gap-2">
+              <a
+                target='_blank'
+                href={`https://wa.me/556799998800?text=Olá vim pelo site e gostaria de mais informações`}
+                className="bg-[#E84C3D] text-white flex items-center justify-center w-fit gap-2 px-4 py-2 rounded-md"
+              >
+                <WhatsappLogo className="w-5 h-5 text-white" />
+                Contato via WhatsApp
+              </a>
+
+              <a
+                href="#"
+                className=" flex items-center justify-center w-fit gap-2 px-4 py-2 rounded-md"
+              >
+                <MapPin className="w-5 h-5 text-black" />
+                Endereço da loja
+              </a>
+            </div>
+
+          </div>
+
+        </div>
+
+
+      </div>
+    </section>
+  )
+}
